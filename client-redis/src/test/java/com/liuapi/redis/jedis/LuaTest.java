91c557f1bc008f4ab9f316d94cd5191b159d036a
johnliu1122 <johnliu1122@163.com>
2020-11-02 13:34:21 +0800

    添加lua脚本

--- client-redis/src/test/java/com/liuapi/redis/jedis/LuaTest.java
+++ client-redis/src/test/java/com/liuapi/redis/jedis/LuaTest.java
@@ -6,6 +6,7 @@ import redis.clients.jedis.Jedis;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Date;
 import java.util.Set;
 
 /**
@@ -41,7 +42,6 @@ public class LuaTest {
      * 秒杀场景下：扣减商品库存
      * 备注
      * 由于lua脚本的限制，无法是使用  redis.call('get',KEYS[1]) >= ARGV[1] 来判断两个整数的大小，因为lua脚本会认为这两个参数都是字符串
-     *
      */
     @Test
     void secondKill() {
@@ -53,14 +53,14 @@ public class LuaTest {
                     " return -1" +
                     " end";
             long lastNumber = 0;
-            do{
-                lastNumber = (long) jedis.eval(lua, Lists.newArrayList("goods1.number"), Lists.newArrayList(buyNumber+""));
-                if(lastNumber>=0){
-                    System.out.println("秒杀成功---> 秒杀数量："+buyNumber+" 剩余数量："+lastNumber);
-                }else{
+            do {
+                lastNumber = (long) jedis.eval(lua, Lists.newArrayList("goods1.number"), Lists.newArrayList(buyNumber + ""));
+                if (lastNumber >= 0) {
+                    System.out.println("秒杀成功---> 秒杀数量：" + buyNumber + " 剩余数量：" + lastNumber);
+                } else {
                     System.out.println("秒杀失败（无库存）");
                 }
-            }while(lastNumber>=buyNumber);
+            } while (lastNumber >= buyNumber);
         }
         System.out.println("--------end--------");
     }
@@ -100,7 +100,7 @@ public class LuaTest {
     }
 
     @Test
-    void testLuaSha1(){
+    void testLuaSha1() {
         String lua = "if (redis.call('get',KEYS[1])-ARGV[1])>=0 then" +
                 " return redis.call('decrby',KEYS[1],ARGV[1])" +
                 " else" +
@@ -112,24 +112,46 @@ public class LuaTest {
 
             int buyNumber = 10;
             long lastNumber = 0;
-            lastNumber = (long)jedis.evalsha(sha1, Lists.newArrayList("goods1.number"), Lists.newArrayList(buyNumber + ""));
+            lastNumber = (long) jedis.evalsha(sha1, Lists.newArrayList("goods1.number"), Lists.newArrayList(buyNumber + ""));
             System.out.println(lastNumber);
         }
     }
 
     @Test
-    void executeCheckAndGetAndSet(){
+    void executeCheckAndGetAndSet() {
         String lua = "if (redis.call('scard',KEYS[1]))>0 then" +
-                " local b = redis.call('smembers',KEYS[1]);"+
-                " redis.call('del',KEYS[1]);"+
+                " local b = redis.call('smembers',KEYS[1]);" +
+                " redis.call('del',KEYS[1]);" +
                 " return b" +
                 " else" +
                 " return nil" +
                 " end";
         try (Jedis jedis = new Jedis("localhost");) {
-            ArrayList<String> sets= (ArrayList<String>)jedis.eval(
+            ArrayList<String> sets = (ArrayList<String>) jedis.eval(
                     lua, Lists.newArrayList("session:loop0"), Collections.emptyList());
             System.out.println(sets);
+        }
+    }
+
+    @Test
+    void existsAndCheckAndSet() {
+        long time = System.currentTimeMillis();
+        System.out.println(time);
+
+        String lua = "if (redis.call('exists',KEYS[1]) ==0) then " +
+                "redis.call('set',KEYS[1],ARGV[1]);" +
+                "return 1;" +
+                "end;" +
+                "if (redis.call('get',KEYS[1])-ARGV[1] <0) then " +
+                " redis.call('set',KEYS[1],ARGV[1]);" +
+                " return 1;" +
+                " else" +
+                " return 0;" +
+                " end;";
+        try (Jedis jedis = new Jedis("localhost");) {
+            long verify = (long) jedis.eval(
+                    lua, Lists.newArrayList("ide:appcode2"), Lists.newArrayList(time + ""));
+            System.out.println(verify);
         }
     }
 }
